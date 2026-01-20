99c3c9fa10e898c7b5241c173937ef84be2ed416
talarys <d.simon.ssx0@gmail.com>
2026-01-20 23:46:19 +0100

    Fixed defaults

--- modules/defaults.nix
+++ modules/defaults.nix
@@ -6,42 +6,42 @@
 }:
 {
   den.default = {
+    nixos.system.stateVersion = "25.05";
+    homeManager.home.stateVersion = "25.05";
+
     nixos = {
       imports = [ inputs.nix-index-database.nixosModules.nix-index ];
-      system.stateVersion = "25.05";
       nixpkgs.config.allowUnfree = true;
       programs.nix-index-database.comma.enable = true;
+
+      nix = {
+        settings = {
+          warn-dirty = false;
+          extra-system-features = [ "recursive-nix" ];
+          experimental-features = [
+            "nix-command"
+            "flakes"
+            "pipe-operators"
+            "recursive-nix"
+          ];
+          trusted-users = [
+            "root"
+            "@wheel"
+          ];
+          substituters = [
+            "https://cache.nixos.org/"
+            "https://nix-community.cachix.org"
+          ];
+          trusted-public-keys = [
+            "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+            "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+          ];
+        };
+      };
     };
 
     homeManager = {
-      home.stateVersion = "25.05";
-    };
-
-    systemd.user.startServices = "sd-switch";
-
-    nix = {
-      settings = {
-        warn-dirty = false;
-        extra-system-features = [ "recursive-nix" ];
-        experimental-features = [
-          "nix-command"
-          "flakes"
-          "pipe-operators"
-          "recursive-nix"
-        ];
-        trusted-users = [
-          "root"
-          "@wheel"
-        ];
-        substituters = [
-          "https://cache.nixos.org/"
-          "https://nix-community.cachix.org"
-        ];
-        trusted-public-keys = [
-          "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        ];
-      };
+      systemd.user.startServices = "sd-switch";
     };
 
   };
