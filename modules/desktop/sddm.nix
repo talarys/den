ebd488c82fe0ea2d5b08c3e0f82d16c95dbb8b66
talarys <d.simon.ssx0@gmail.com>
2026-01-26 18:16:48 +0000

    Added catppuccin theme

--- modules/desktop/sddm.nix
+++ modules/desktop/sddm.nix
@@ -1,11 +1,21 @@
 {
   den.aspects.desktop._.sddm.nixos =
     { pkgs, ... }:
+    let
+      flavor = "mocha";
+      accent = "lavender";
+    in
     {
+      environment.systemPackages = [
+        (pkgs.catppuccin-sddm.override {
+          flavor = "mocha";
+          accent = "lavender";
+        })
+      ];
       services.displayManager.sddm = {
         enable = true;
         extraPackages = [ pkgs.sddm-astronaut ];
-        theme = "sddm-astronaut-theme";
+        theme = "catppuccin-${flavor}-${accent}";
         wayland.enable = true;
       };
     };
