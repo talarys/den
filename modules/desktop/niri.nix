d35e0ab9efa67703d9d26fd34926d2f3b2bf0140
talarys <d.simon.ssx0@gmail.com>
2026-02-06 23:06:24 +0000

    Removed deprecated option

--- modules/desktop/niri.nix
+++ modules/desktop/niri.nix
@@ -14,7 +14,6 @@
 
         # Core features
         enableSystemMonitoring = true; # System monitoring widgets (dgop)
-        enableClipboard = true; # Clipboard history manager
         enableVPN = true; # VPN management widget
         enableDynamicTheming = true; # Wallpaper-based theming (matugen)
         enableAudioWavelength = true; # Audio visualizer (cava)
