eb91355c2aa9b767482b474037b6c209df9fc9bd
talarys <d.simon.ssx0@gmail.com>
2026-02-07 17:14:03 +0100

    Mpv Config

---
+++ modules/apps/mpv/mpv.nix
@@ -0,0 +1,28 @@
+{
+  den.aspects.apps._.mpv.homeManager =
+    { pkgs, ... }:
+    {
+      programs.mpv = {
+        enable = true;
+        scripts = with pkgs.mpvScripts; [
+          uosc
+          thumbfast
+          mpv-cheatsheet
+          evafast
+          dynamic-crop
+          quality-menu
+          eisa01.simplehistory
+          eisa01.simplebookmark
+          eisa01.smart-copy-paste-2
+        ];
+      };
+
+      home.file = {
+        ".config/mpv/shaders".source = ./shaders;
+        ".config/mpv/fonts".source = ./fonts;
+        ".config/mpv/input.conf".source = ./input.conf;
+        ".config/mpv/inputevent_key.conf".source = ./inputevent_key.conf;
+        ".config/mpv/mpv.conf".source = ./mpv.conf;
+      };
+    };
+}
