8078c6e1ae8fbf641f0d5877239d1b72ba9cf5e8
talarys <d.simon.ssx0@gmail.com>
2026-06-08 17:34:22 +0000

    Added binary cache for cachyos kernels

--- modules/defaults.nix
+++ modules/defaults.nix
@@ -44,9 +44,11 @@
           ];
           substituters = [
             "https://nix-community.cachix.org"
+            "https://attic.xuyh0120.win/lantian"
           ];
           trusted-public-keys = [
             "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+            "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
           ];
         };
       };
