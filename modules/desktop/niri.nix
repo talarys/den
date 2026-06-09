aa36b7c997c64c79229ce6b96460cf664b819902
talarys <d.simon.ssx0@gmail.com>
2026-06-09 21:18:56 +0200

    Update niri config

--- modules/desktop/niri.nix
+++ modules/desktop/niri.nix
@@ -1,24 +1,41 @@
 {
-  den.aspects.desktop._.niri.nixos = {
-    programs = {
-      niri = {
-        enable = true;
-      };
-      dms-shell = {
-        enable = true;
+  den.aspects.desktop._.niri.nixos =
+    { pkgs, ... }:
+    {
+      environment.systemPackages = with pkgs; [
+        xdg-desktop-portal-gtk
+        xdg-desktop-portal-gnome
+        gnome-keyring
+        cups-pk-helper
+        xwayland-satellite
+        fuzzel
+      ];
 
-        systemd = {
-          enable = true; # Systemd service for auto-start
-          restartIfChanged = true; # Auto-restart dms.service when dms-shell changes
+      services.displayManager.dms-greeter = {
+        enable = true;
+        compositor.name = "niri";
+      };
+
+      programs = {
+        niri = {
+          enable = true;
         };
 
-        # Core features
-        enableSystemMonitoring = true; # System monitoring widgets (dgop)
-        enableVPN = true; # VPN management widget
-        enableDynamicTheming = true; # Wallpaper-based theming (matugen)
-        enableAudioWavelength = true; # Audio visualizer (cava)
-        enableCalendarEvents = true; # Calendar integration (khal)
+        dms-shell = {
+          enable = true;
+
+          systemd = {
+            enable = true; # Systemd service for auto-start
+            restartIfChanged = true; # Auto-restart dms.service when dms-shell changes
+          };
+
+          # Core features
+          enableSystemMonitoring = true; # System monitoring widgets (dgop)
+          enableVPN = true; # VPN management widget
+          enableDynamicTheming = true; # Wallpaper-based theming (matugen)
+          enableAudioWavelength = true; # Audio visualizer (cava)
+          enableCalendarEvents = true; # Calendar integration (khal)
+        };
       };
     };
-  };
 }
