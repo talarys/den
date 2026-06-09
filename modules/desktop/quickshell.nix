5546dd31690ca701e564ed2e5543eba130e886bc
talarys <d.simon.ssx0@gmail.com>
2026-06-09 21:16:36 +0200

    Update quickshell

--- modules/desktop/quickshell.nix
+++ modules/desktop/quickshell.nix
@@ -5,27 +5,16 @@
       environment.systemPackages = with pkgs; [
         quickshell
 
-        # Qt6 related kits（for slove Qt5Compat problem）
+        # Qt6 dependencies for Quickshell
         qt6.qt5compat
         qt6.qtbase
         qt6.qtquick3d
         qt6.qtwayland
         qt6.qtdeclarative
         qt6.qtsvg
-
-        # alternate options
-        # libsForQt5.qt5compat
-        kdePackages.qt5compat
-        libsForQt5.qt5.qtgraphicaleffects
       ];
 
-      # necessary environment variables
-      environment.variables = {
-        QML_IMPORT_PATH = "${pkgs.qt6.qt5compat}/lib/qt-6/qml:${pkgs.qt6.qtbase}/lib/qt-6/qml";
-        QML2_IMPORT_PATH = "${pkgs.qt6.qt5compat}/lib/qt-6/qml:${pkgs.qt6.qtbase}/lib/qt-6/qml";
-      };
-
-      # make sure the Qt application is working properly
+      # Environment variables for Qt and Wayland
       environment.sessionVariables = {
         QT_QPA_PLATFORM = "wayland;xcb";
         QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
