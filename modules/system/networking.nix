90b51079275a1e3b5ce6040ac12c9de881792d24
talarys <d.simon.ssx0@gmail.com>
2026-06-10 18:45:45 +0200

    Opened some ports on the firewall

--- modules/system/networking.nix
+++ modules/system/networking.nix
@@ -2,6 +2,17 @@
   den.aspects.system._.networking = {
     nixos = {
       networking = {
+        firewall = {
+          enable = true;
+          allowedTCPPorts = [
+            80
+            443
+            3000
+            5173
+            8080
+          ];
+          allowedUDPPorts = [ 3377 ];
+        };
         networkmanager.enable = true;
         wireless.enable = true;
         nameservers = [
