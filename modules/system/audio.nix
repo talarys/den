f1f21a3acf9eb44d5912a558e04a4ca60a4c79a1
talarys <d.simon.ssx0@gmail.com>
2026-06-12 20:42:56 +0200

    Audio fix

--- modules/system/audio.nix
+++ modules/system/audio.nix
@@ -10,6 +10,8 @@
       services.pipewire = {
         enable = true;
         audio.enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
         pulse.enable = true;
         jack.enable = true;
       };
